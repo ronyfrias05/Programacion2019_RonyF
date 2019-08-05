842b59f8d6531d84672ed5b8bd101e84a1da13a5
RonyF <ronyfreezer@gmail.com>
2019-08-05 12:17:35 -0400

    Condicionales if

--- modulo_1/intro_javascript/ejercicio2_objeto_sitio/js/hotel.js
+++ modulo_1/intro_javascript/ejercicio2_objeto_sitio/js/hotel.js
@@ -26,4 +26,27 @@ console.log(hotel.tieneGym);
 console.log(hotel.habitaciones);
 
 // Concatenar vs Template string
-console.log("Bienvenido al hotel " + hotel.nombre + " que tiene " + hotel.rating + " de rating");n
+console.log(`Hola, soy un templete string`);
+console.log("Bienvenido al hotel " + hotel.nombre + " que tiene " + hotel.rating + " de rating");
+
+console.log("El hotel tiene " + hotel.reservaciones);
+console.log(`El hotel tiene ${hotel.reservaciones}`);
+
+console.log("El hotel tine " + hotel.habitaciones);
+console.log(`El hotel tiene ${hotel.habitaciones}`);
+
+console.log("El hotel tiene " + hotel.reservaciones + " de " + hotel.habitaciones + " habitaciones");
+console.log(`El hotel tiene ${hotel.reservaciones} de ${hotel.habitaciones} habitaciones`);
+
+console.log("El hotel tiene " + (hotel.habitaciones - hotel.reservaciones) + " habitaciones disponibles");
+console.log(`El hotel tiene ${hotel.habitaciones - hotel.reservaciones} habitacionens disponibles`);
+
+console.log(hotel.nombre + " posee un rating de " + hotel.rating + ". Posee " + hotel.habitaciones + ".  Posee " + hotel.reservaciones + ".");
+console.log(`${hotel.nombre} posee un rating de ${hotel.rating}. Posee ${hotel.habitaciones}. Posee ${hotel.reservaciones}.`);
+
+// La linea de que esta abajo es un templete string, porque se usa este caracater: (`) y para las variables o expresiones: ${}
+console.log(`El hotel tiene ${hotel.habitaciones - hotel.reservaciones} habitaciones disponibles`);
+
+document.write(`<h1>Bienvenidos a ${hotel.nombre}</h1>
+<p>Este hotel es de ${hotel.rating} estrellas</p>
+<p>Actualmente tenemos ${hotel.habitaciones} de las cuales ${hotel.reservaciones} estan reservadas.</p>`);
---
+++ modulo_1/intro_javascript/ejercicio3_esctrutura_control/js/edad.js
@@ -0,0 +1,65 @@
+var estudiante = {
+    nombre: "Antonio",
+    edad: 18,
+    tieneHijos: false,
+    tieneCarro: true,
+    estaCasado: false,
+}
+/*
+if (estudiante.edad <= 18) {
+    console.log(`${estudiante.nombre} es mayor de edad`);
+}
+
+if (estudiante.edad >= 18) {
+    console.log(`${estudiante.nombre} es mayor de edad`);
+}
+
+var abierto = true;
+var cerrado = false;
+
+if (cerrado) {
+    console.log(`Esta abierto`);
+} else {
+    console.log("Esta cerrado");
+}
+
+if (estudiante.edad >= 18) {
+    console.log("Es mayor de edad")
+} else {
+    console.log("Es menor de edad");
+}
+*/
+
+if (estudiante.estaCasado) {
+    console.log("Si estas casado")
+} else {
+    console.log("No estas casado");
+}
+
+if (estudiante.tieneCarro) {
+    console.log("Si tienes carro")
+} else {
+    console.log("No tienes carro");
+}
+
+if (estudiante.tieneHijos) {
+    console.log("Si tienes hijos")
+} else {
+    console.log("No tienes hijos");
+}
+
+console.log(10 + 3); // Resultado
+console.log(3 * 6); // Resultado 
+console.log(7 - 6); // Resultado 
+console.log(8 < 0); // Resultado 
+console.log(1 < 0); // Resultado 
+console.log(0 > 0); // Resultado 
+console.log(15 >= 15); // Resultado 
+console.log(10 <= 10); // Resultado 
+console.log(3 < 3); // Resultado 
+console.log(-1 > 5); // Resultado 
+console.log(-10 > 10); // Resultado 
+console.log(10 > -10); // Resultado 
+console.log(10 >= 10); // Resultado 
+console.log(7 * 7); // Resultado 
+console.log(1 < 1); // Resultado 
