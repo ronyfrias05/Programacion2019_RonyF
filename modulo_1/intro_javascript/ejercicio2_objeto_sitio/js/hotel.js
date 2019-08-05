31cd355b8a30673226af3fab1184635fdfc70912
RonyF <ronyfreezer@gmail.com>
2019-08-05 08:59:19 -0400

    Guardado Inicial con la configuracion

---
+++ modulo_1/intro_javascript/ejercicio2_objeto_sitio/js/hotel.js
@@ -0,0 +1,11 @@
+var nombreHotel = "DominguerosCold";
+var ratingHotel = 5;
+var habitacionesHotel = 10;
+var reservacionesHotel = 9;
+var tieneGymHotel = true;
+
+console.log(nombreHotel);
+console.log(ratingHotel);
+console.log(habitacionesHotel);
+console.log(reservacionesHotel);
+console.log(tieneGymHotel);
