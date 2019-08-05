1867eb2360217773798bd40b09d76919702b7664
RonyF <ronyfreezer@gmail.com>
2019-08-05 09:59:21 -0400

    guardado rapido

--- modulo_1/intro_javascript/ejercicio2_objeto_sitio/js/hotel.js
+++ modulo_1/intro_javascript/ejercicio2_objeto_sitio/js/hotel.js
@@ -1,7 +1,8 @@
-var nombreHotel = "DominguerosCold";
+/*
+var nombreHotel = "Domingueros Cold";
 var ratingHotel = 5;
-var habitacionesHotel = 10;
-var reservacionesHotel = 9;
+var habitacionesHotel = 499;
+var reservacionesHotel = 498;
 var tieneGymHotel = true;
 
 console.log(nombreHotel);
@@ -9,3 +10,20 @@ console.log(ratingHotel);
 console.log(habitacionesHotel);
 console.log(reservacionesHotel);
 console.log(tieneGymHotel);
+*/
+var hotel = {
+    nombre: "Domingueros Cold",
+    rating: 5,
+    habitaciones: 499,
+    reservaciones: 498,
+    tieneGym: true,
+}
+
+console.log(hotel.nombre);
+console.log(hotel.rating);
+console.log(hotel.reservaciones);
+console.log(hotel.tieneGym);
+console.log(hotel.habitaciones);
+
+// Concatenar vs Template string
+console.log("Bienvenido al hotel " + hotel.nombre + " que tiene " + hotel.rating + " de rating");n
