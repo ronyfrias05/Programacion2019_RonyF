2fca95165ff8589983f68af86d6b905acb49e6f8
RonyF <ronyfreezer@gmail.com>
2019-08-06 12:16:07 -0400

    tercer guardado del dia

--- modulo_1/intro_javascript/ejercicio3_esctrutura_control/js/odc.js
+++ modulo_1/intro_javascript/ejercicio3_esctrutura_control/js/odc.js
@@ -8,10 +8,8 @@ console.log(11 % 3 ** 2);   // 2
 
 // Ejercicio 1
 // Digita 2 numeros y mostra su suma, resta, multiplicacion, division, potencia y residuo.
-var numero = parseInt( prompt("Digite su primer numero") );
-var numeros = parseInt( prompt("Digite su segundo numero") );
+
 /*
-console.log(`El primer numero es ${numero} y el segundo es ${numeros}`);
 console.log(`${numero} + ${numeros} = ${numero + numeros}`);
 console.log(`${numero} - ${numeros} = ${numero - numeros}`);
 console.log(`${numero} * ${numeros} = ${numero * numeros}`);
@@ -19,6 +17,7 @@ console.log(`${numero} / ${numeros} = ${numero / numeros}`);
 console.log(`${numero} ** ${numeros} = ${numero ** numeros}`);
 console.log(`${numero} % ${numeros} = ${numero % numeros}`);
 */
+/*
 // 1
 if (numero == numeros) {
     console.log(`Los numeros son iguales`);
@@ -33,7 +32,7 @@ if (numero > numeros) {
 }
 // 4
 if (numeros > numero) {
-    console.log(`El primer numero es menor que el segundo`);
+    console.log(`El segundo numero es mayor que el primero`);
 }
 // 5
 if (numero < numeros) {
@@ -51,3 +50,26 @@ if (numero >= numeros) {
 if (numeros >= numero) {
     console.log(`El segundo numero es mayor o igual que el primero`);
 }
+*/
+
+/*
+var numero1 = parseInt( prompt("Digite su primer numero") );
+var numero2 = parseInt( prompt("Digite su segundo numero") );
+
+if (numero1 > numero2) {
+    console.log(`El primer numero ${numero1} es mayor que el segundo numero ${numero2}`);
+} else if (numero1 < numero2) {
+    console.log(`El primer numero ${numero1} es menor que el segundo numero ${numero2}`);
+}
+*/
+/*
+if (numero1 > numero2) {
+    console.log(`${numero1} es el mayor`);
+} else {
+    console.log(`${numero2} es el mayor`);
+}
+*/
+
+// === Mejor forma !== mejor forma
+console.log("10" !== 10);
+console.log("5" !== 5);
