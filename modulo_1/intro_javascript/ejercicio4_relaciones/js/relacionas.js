1badee347208b3375dd68a68ddab5282d5736c57
RonyF <ronyfreezer@gmail.com>
2019-08-09 12:23:14 -0400

    operadores relacionales o apersan

---
+++ modulo_1/intro_javascript/ejercicio4_relaciones/js/relacionas.js
@@ -0,0 +1,23 @@
+/*
+var carro = {
+    tieneBateria: false,
+    tieneLlave: false,
+}
+
+if (carro.tieneBateria && carro.tieneLlave) {
+    console.log("El carro va a encender.");
+} else {
+    console.log("No enciende.")
+}
+*/
+
+var mesa = {
+    hayCuchara: true,
+    hayTenedor: false,
+}
+
+if (mesa.hayCuchara || mesa.hayTenedor) {
+    console.log("Yo puedo comer :D");
+} else {
+    console.log("No puedo comer :(");
+}
