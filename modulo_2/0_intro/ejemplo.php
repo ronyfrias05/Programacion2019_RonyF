e33fd875990b764d68a50b92873a30b3c77b0fbf
RonyF <nikol123NIKOL>
2019-08-20 12:18:33 -0400

    modulo2

---
+++ modulo_2/0_intro/ejemplo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Soy PHP</title>
+</head>
+<body>
+    <h1>Soy PHP</h1>
+    <?php
+    $nombre = "Rony";
+
+    echo"Bienvenido " . $nombre;
+    ?>
+</body>
+</html>
