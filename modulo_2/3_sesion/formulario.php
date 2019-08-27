bdb78afa476414ed509ed80de50889eb99bf7c99
RonyF <ronyfreezer@gmail.com>
2019-08-27 12:23:34 -0400

    commit

--- modulo_2/3_sesion/formulario.php
+++ modulo_2/3_sesion/formulario.php
@@ -1,3 +1,7 @@
+<?php
+ session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -6,11 +10,12 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Formulario</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
 </head>
 
 <body>
 
-    <div class="container my-5 border">
+    <div class="container my-5 border animated fadeInDownBig">
         <form class="" method="post">
             <div class="form-group mt-4">
                 <label for="" class="display-4">Publicar</label>
@@ -19,23 +24,29 @@
             </div>
                 <button type="submit" class="btn btn-outline-secondary my-3" name="guardar">Guardar publicacion</button>
         </form>
+        <a href="listado.php">Ir al listado</a>
         <hr>
 
     <!-- http://www.lorempixel.com/300/300/ -->
 
     <?php
-
+        if (!isset($_SESSION['publicaciones'])) {
+            $_SESSION['publicaciones'] = [];
+        }
 
         if (isset($_POST['guardar'])) {
-            // Objeto
             $datos = new stdClass();
             $datos->texto = $_POST['texto'];
             $datos->url = $_POST['url'];
-            
-            echo "<pre>";
-            print_r($datos);
-            echo "</pre>";
-            
+
+            // Agregar los datos a la SESSION
+            $resultado = array_push($_SESSION['publicaciones'], $datos);
+
+            if ($resultado) {
+                echo "<p class='alert alert-success'>Se agregó</p>";
+            } else {
+                echo "<p class='alert alert-danger'>No se agregó</p>";
+            }
         }
 
     ?>
--- modulo_2/3_sesion/listado.php
+++ modulo_2/3_sesion/listado.php
@@ -1,3 +1,9 @@
+<?php
+    session_start();
+
+    // $_SESSION = [];
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -6,12 +12,20 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Listado</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
 </head>
 <body>
     
-    <?php
-    
+    <div class="row">
+    <?php 
+        // Recorrer las publicaciones
+            if (isset($_SESSION['publicaciones'])) {
+                foreach ($_SESSION['publicaciones'] as $publicacion) {
+                    echo "<div class='col-md-4 border my-5 container'><img src='{$publicacion->url}' class='img-thumbnail w-100 my-3 animated fadeIn' alt='imagen' style='width: 100%'><p class=' text-center'>{$publicacion->texto}</p></div>";
+                }        
+            }
     ?>
+    </div>
 
 </body>
 </html>
