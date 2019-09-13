a1d30b34c4ea6702bb457be6ab7a2addf1af2f25
RonyF <ronyfreezer@gmail.com>
2019-09-13 12:19:57 -0400

    commint

--- modulo_2/10_registro_de_datos/conexion.php
+++ modulo_2/10_registro_de_datos/conexion.php
@@ -5,11 +5,14 @@ try {
     $DB_NAME = "productos";
     $DB_USER = "root";
     $DB_PASS = "";
-    $config = [
-         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ];
+    // $config = [
+    //     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    // ];
 
-   
+    $config = [
+        3 => 2, // Lanzar  execepiones como errores
+        19 => 5, // Traer los datos en forma de objetos
+    ];
 
     $conexion = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PASS, $config);
 } catch (PDOExeception $pdo) {
--- modulo_2/10_registro_de_datos/vistas/lista_cartas.html.php
+++ modulo_2/10_registro_de_datos/vistas/lista_cartas.html.php
@@ -29,14 +29,14 @@
 
             foreach ($datos as $dato) {
                 echo '<div class="card col-md-3 animated fadeIn delay-1s m-2">
-                            <img src="' . $dato['link'] . '" alt="" class="card-img-top img-thumbnail mt-3 p-2">
+                            <img src="' . $dato->link . '" alt="" class="card-img-top img-thumbnail mt-3 p-2">
                             <div class="card-body">
-                                <h5 class="card-title">' . $dato['name'] . '</h5>
-                                <p class="card-text text-success">$ ' . $dato['price'] . '</p>
-                                <a href="ver_carta.php?id='.$dato['id'].'" class="btn btn-outline-primary">Ver producto</a>
+                                <h5 class="card-title">' . $dato->name . '</h5>
+                                <p class="card-text text-success">$ ' . $dato->price . '</p>
+                                <a href="ver_carta.php?id='.$dato->id.'" class="btn btn-outline-primary">Ver producto</a>
 
                                 <form method="post" action="borrar_carta.php">
-                                    <input type="hidden" name="id" value="'.$dato['id'].'">
+                                    <input type="hidden" name="id" value="'.$dato->id.'">
                                     <button class="btn btn-outline-danger d-inline-block btn-sm my-2" name="borrar">Borrar</button>
                                 </form>
                             </div>
