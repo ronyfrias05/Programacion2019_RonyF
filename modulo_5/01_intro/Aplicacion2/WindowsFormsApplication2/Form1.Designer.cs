91f5d1cb9c1da3c5593abecc7db1db0685666313
RonyF <ronyfreezer@gmail.com>
2019-10-08 10:47:50 -0400

    New2019

---
+++ modulo_5/01_intro/Aplicacion2/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.chkAcepto = new System.Windows.Forms.CheckBox();
+            this.btnRegistrarse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(29, 42);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(121, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Edad";
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(29, 95);
+            this.txtEdad.MaxLength = 1;
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(60, 20);
+            this.txtEdad.TabIndex = 3;
+            // 
+            // chkAcepto
+            // 
+            this.chkAcepto.AutoSize = true;
+            this.chkAcepto.Location = new System.Drawing.Point(32, 134);
+            this.chkAcepto.Name = "chkAcepto";
+            this.chkAcepto.Size = new System.Drawing.Size(118, 17);
+            this.chkAcepto.TabIndex = 4;
+            this.chkAcepto.Text = "Acepto los terminos";
+            this.chkAcepto.UseVisualStyleBackColor = true;
+            this.chkAcepto.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // btnRegistrarse
+            // 
+            this.btnRegistrarse.Location = new System.Drawing.Point(29, 172);
+            this.btnRegistrarse.Name = "btnRegistrarse";
+            this.btnRegistrarse.Size = new System.Drawing.Size(100, 22);
+            this.btnRegistrarse.TabIndex = 5;
+            this.btnRegistrarse.Text = "Registrarse";
+            this.btnRegistrarse.UseVisualStyleBackColor = true;
+            this.btnRegistrarse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 332);
+            this.Controls.Add(this.btnRegistrarse);
+            this.Controls.Add(this.chkAcepto);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.CheckBox chkAcepto;
+        private System.Windows.Forms.Button btnRegistrarse;
+    }
+}
+
---
+++ modulo_5/01_intro/Aplicacion2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Acceder al texto del objeto txtNombre
+            var nombre = txtNombre.Text;
+            // Convertir el texto a numero
+            var edad = Convert.ToInt32(txtEdad.Text); 
+
+            // Verificar si es mayor o menor de edad
+            if (edad >= 18) 
+            {
+                // Mayor
+                MessageBox.Show("Eres mayor");
+            }
+            else
+            {
+                // Menor
+                MessageBox.Show("Eres menor");
+            };
+
+            MessageBox.Show(nombre);
+            MessageBox.Show(edad.ToString());
+            MessageBox.Show(nombre + " " + edad);
+            
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
