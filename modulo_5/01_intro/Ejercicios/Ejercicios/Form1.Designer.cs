a3681426dccd1abef7d2b46c9a6eac8ed0d6285d
RonyF <ronyfreezer@gmail.com>
2019-10-18 12:02:09 -0400

    x2

--- modulo_5/01_intro/Ejercicios/Ejercicios/Form1.Designer.cs
+++ modulo_5/01_intro/Ejercicios/Ejercicios/Form1.Designer.cs
@@ -33,27 +33,27 @@
             this.btnVerificar = new System.Windows.Forms.Button();
             this.grNumeros = new System.Windows.Forms.GroupBox();
             this.grColor = new System.Windows.Forms.GroupBox();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.barB = new System.Windows.Forms.TrackBar();
+            this.barG = new System.Windows.Forms.TrackBar();
+            this.barR = new System.Windows.Forms.TrackBar();
+            this.numB = new System.Windows.Forms.NumericUpDown();
+            this.numG = new System.Windows.Forms.NumericUpDown();
+            this.numR = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.btnColor = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.numR = new System.Windows.Forms.NumericUpDown();
-            this.numG = new System.Windows.Forms.NumericUpDown();
-            this.numB = new System.Windows.Forms.NumericUpDown();
-            this.barR = new System.Windows.Forms.TrackBar();
-            this.barG = new System.Windows.Forms.TrackBar();
-            this.barB = new System.Windows.Forms.TrackBar();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.progressBar2 = new System.Windows.Forms.ProgressBar();
-            this.progressBar3 = new System.Windows.Forms.ProgressBar();
             this.grNumeros.SuspendLayout();
             this.grColor.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numR)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numG)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numB)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.barR)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.barG)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.barB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barG)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barR)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numG)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numR)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -123,6 +123,135 @@
             this.grColor.TabStop = false;
             this.grColor.Text = "Cambiar color de fondo";
             // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(451, 117);
+            this.progressBar3.Maximum = 255;
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(134, 23);
+            this.progressBar3.TabIndex = 13;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(451, 70);
+            this.progressBar2.Maximum = 255;
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(134, 23);
+            this.progressBar2.TabIndex = 12;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(451, 24);
+            this.progressBar1.Maximum = 255;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(134, 23);
+            this.progressBar1.TabIndex = 11;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // barB
+            // 
+            this.barB.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.barB.Location = new System.Drawing.Point(201, 116);
+            this.barB.Maximum = 255;
+            this.barB.Name = "barB";
+            this.barB.Size = new System.Drawing.Size(244, 45);
+            this.barB.TabIndex = 10;
+            this.barB.Scroll += new System.EventHandler(this.barB_Scroll);
+            // 
+            // barG
+            // 
+            this.barG.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.barG.Location = new System.Drawing.Point(201, 70);
+            this.barG.Maximum = 255;
+            this.barG.Name = "barG";
+            this.barG.Size = new System.Drawing.Size(244, 45);
+            this.barG.TabIndex = 9;
+            this.barG.Scroll += new System.EventHandler(this.barG_Scroll);
+            // 
+            // barR
+            // 
+            this.barR.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.barR.Location = new System.Drawing.Point(201, 19);
+            this.barR.Maximum = 255;
+            this.barR.Name = "barR";
+            this.barR.Size = new System.Drawing.Size(244, 45);
+            this.barR.TabIndex = 8;
+            this.barR.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // numB
+            // 
+            this.numB.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.numB.Location = new System.Drawing.Point(75, 118);
+            this.numB.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numB.Name = "numB";
+            this.numB.Size = new System.Drawing.Size(120, 22);
+            this.numB.TabIndex = 7;
+            this.numB.Value = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            // 
+            // numG
+            // 
+            this.numG.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.numG.Location = new System.Drawing.Point(75, 70);
+            this.numG.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numG.Name = "numG";
+            this.numG.Size = new System.Drawing.Size(120, 22);
+            this.numG.TabIndex = 6;
+            this.numG.Value = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            // 
+            // numR
+            // 
+            this.numR.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.numR.Location = new System.Drawing.Point(75, 24);
+            this.numR.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numR.Name = "numR";
+            this.numR.Size = new System.Drawing.Size(120, 22);
+            this.numR.TabIndex = 5;
+            this.numR.Value = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(43, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "B:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(43, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "G:";
+            // 
             // btnColor
             // 
             this.btnColor.BackColor = System.Drawing.Color.White;
@@ -147,135 +276,6 @@
             this.label2.TabIndex = 0;
             this.label2.Text = "R:";
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(43, 72);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(22, 16);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "G:";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(43, 120);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(21, 16);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "B:";
-            // 
-            // numR
-            // 
-            this.numR.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.numR.Location = new System.Drawing.Point(75, 24);
-            this.numR.Maximum = new decimal(new int[] {
-            255,
-            0,
-            0,
-            0});
-            this.numR.Name = "numR";
-            this.numR.Size = new System.Drawing.Size(120, 22);
-            this.numR.TabIndex = 5;
-            this.numR.Value = new decimal(new int[] {
-            255,
-            0,
-            0,
-            0});
-            // 
-            // numG
-            // 
-            this.numG.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.numG.Location = new System.Drawing.Point(75, 70);
-            this.numG.Maximum = new decimal(new int[] {
-            255,
-            0,
-            0,
-            0});
-            this.numG.Name = "numG";
-            this.numG.Size = new System.Drawing.Size(120, 22);
-            this.numG.TabIndex = 6;
-            this.numG.Value = new decimal(new int[] {
-            255,
-            0,
-            0,
-            0});
-            // 
-            // numB
-            // 
-            this.numB.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.numB.Location = new System.Drawing.Point(75, 118);
-            this.numB.Maximum = new decimal(new int[] {
-            255,
-            0,
-            0,
-            0});
-            this.numB.Name = "numB";
-            this.numB.Size = new System.Drawing.Size(120, 22);
-            this.numB.TabIndex = 7;
-            this.numB.Value = new decimal(new int[] {
-            255,
-            0,
-            0,
-            0});
-            // 
-            // barR
-            // 
-            this.barR.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.barR.Location = new System.Drawing.Point(201, 21);
-            this.barR.Maximum = 255;
-            this.barR.Name = "barR";
-            this.barR.Size = new System.Drawing.Size(203, 45);
-            this.barR.TabIndex = 8;
-            this.barR.Scroll += new System.EventHandler(this.trackBar1_Scroll);
-            // 
-            // barG
-            // 
-            this.barG.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.barG.Location = new System.Drawing.Point(201, 70);
-            this.barG.Maximum = 255;
-            this.barG.Name = "barG";
-            this.barG.Size = new System.Drawing.Size(203, 45);
-            this.barG.TabIndex = 9;
-            this.barG.Scroll += new System.EventHandler(this.barG_Scroll);
-            // 
-            // barB
-            // 
-            this.barB.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.barB.Location = new System.Drawing.Point(201, 116);
-            this.barB.Maximum = 255;
-            this.barB.Name = "barB";
-            this.barB.Size = new System.Drawing.Size(203, 45);
-            this.barB.TabIndex = 10;
-            this.barB.Scroll += new System.EventHandler(this.barB_Scroll);
-            // 
-            // progressBar1
-            // 
-            this.progressBar1.Location = new System.Drawing.Point(411, 24);
-            this.progressBar1.Maximum = 255;
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(100, 23);
-            this.progressBar1.TabIndex = 11;
-            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
-            // 
-            // progressBar2
-            // 
-            this.progressBar2.Location = new System.Drawing.Point(411, 68);
-            this.progressBar2.Maximum = 255;
-            this.progressBar2.Name = "progressBar2";
-            this.progressBar2.Size = new System.Drawing.Size(100, 23);
-            this.progressBar2.TabIndex = 12;
-            // 
-            // progressBar3
-            // 
-            this.progressBar3.Location = new System.Drawing.Point(411, 116);
-            this.progressBar3.Maximum = 255;
-            this.progressBar3.Name = "progressBar3";
-            this.progressBar3.Size = new System.Drawing.Size(100, 23);
-            this.progressBar3.TabIndex = 13;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -291,12 +291,12 @@
             this.grNumeros.PerformLayout();
             this.grColor.ResumeLayout(false);
             this.grColor.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numR)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numG)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numB)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.barR)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.barG)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.barB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barG)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barR)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numG)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numR)).EndInit();
             this.ResumeLayout(false);
 
         }
